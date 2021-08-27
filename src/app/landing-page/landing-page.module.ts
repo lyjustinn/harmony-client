9b22f4571ed6edd16e4e68bcd8c2020d15dfcad5
lyjustinn <87103438+lyjustinn@users.noreply.github.com>
2021-08-26 21:10:11 -0400

    Landing page styling

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     LandingPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     LandingPageComponent
--- src/app/landing-page/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUser } from 'src/app/user/shared/user.model';
+import { UserService } from 'src/app/user/shared/user.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -7,11 +9,19 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  loginUrl : string = environment.apiUrl + "/api/auth"
+  loginUrl : string = environment.apiUrl + "/api/auth";
+  currentUser : CurrentUser;
+  isSignedIn : boolean = false;
 
-  constructor() { }
+  constructor(private userService : UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUser().subscribe( user => {
+      if ( user !== null ) {
+        this.currentUser = user;
+        this.isSignedIn = true;
+      } 
+    })
   }
 
 }
